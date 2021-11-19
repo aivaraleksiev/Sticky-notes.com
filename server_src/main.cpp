8c48a44fc51da5006cfd875dd1659fa4168a1398
Aivar Aleksiev <aivar.aleksiev@gmail.com>
2021-11-19 20:37:28 +0200

    Some refactoring regarding request handling

--- server_src/main.cpp
+++ server_src/main.cpp
@@ -8,6 +8,7 @@
 
 #include <restinio/all.hpp>
 #include <restinio/sync_chain/fixed_size.hpp>
+#include <restinio/sync_chain/growable_size.hpp>
 #include <restinio/tls.hpp>
 
 #include <iostream>
@@ -20,17 +21,21 @@ int main()
 			restinio::single_thread_tls_traits_t<
 			restinio::asio_timer_manager_t,
 			Notes::Utils::Logger,
-			restinio::sync_chain::fixed_size_chain_t<3>>; //todo  try with growable_size_chain_t
+			restinio::sync_chain::growable_size_chain_t<>>;
+		 
+		//todo  make some refactoring and add handlers in endpoint code.
+		restinio::sync_chain::growable_size_chain_t<>::builder_t chain_builder;
+
+		chain_builder.add(Notes::NotesEndpoint::getInstance()->createNoteEndpointRequestHandler());
+		chain_builder.add(Notes::MainEndpoint::getInstance()->createMainEndpointRequestHandler());
+		chain_builder.add(Notes::UserEndpoint::getInstance()->createUserEndpointRequestHandler());
 
 		restinio::run(
 			restinio::on_this_thread<traits_t>()
 			.port(9066)
 			.address("localhost")
 			.logger(Notes::Utils::createLogger("restinio"))
-			.request_handler(
-				Notes::NotesEndpoint::getInstance()->createNoteEndpointRequestHandler(),
-				Notes::MainEndpoint::getInstance()->createMainEndpointRequestHandler(),
-				Notes::UserEndpoint::getInstance()->createUserEndpointRequestHandler())
+			.request_handler(chain_builder.release())
 			.tls_context(std::move(Notes::Utils::createTlsContext())));
 	}
 	catch (const std::exception& ex)
