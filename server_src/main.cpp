7a3598f93be4e951f244c7f44ac96f622df06474
Aivar Aleksiev <aivar.aleksiev@gmail.com>
2021-11-19 17:49:57 +0200

    Secure server using SSL/TLS

--- server_src/main.cpp
+++ server_src/main.cpp
@@ -8,6 +8,7 @@
 
 #include <restinio/all.hpp>
 #include <restinio/sync_chain/fixed_size.hpp>
+#include <restinio/tls.hpp>
 
 #include <iostream>
 
@@ -21,10 +22,21 @@ int main()
 		auto restinioLogger = Notes::Utils::createLogger("restinio");
 
 		using traits_t =
-			restinio::traits_t<
+			restinio::single_thread_tls_traits_t<
 			restinio::asio_timer_manager_t,
 			Notes::Utils::Logger,
 			restinio::sync_chain::fixed_size_chain_t<3>>; //todo  try with growable_size_chain_t
+
+		restinio::asio_ns::ssl::context tls_context{ restinio::asio_ns::ssl::context::sslv23 };
+		tls_context.set_options(
+			restinio::asio_ns::ssl::context::default_workarounds
+			| restinio::asio_ns::ssl::context::no_sslv2);
+		// todo remove hardcoded path.
+		// Variant 1: use configuration file.
+		// Variant 2: generate certs in memory.
+		tls_context.use_certificate_chain_file("D:/My Projects/Github/Sticky-notes.com/server_src/certificates/cacert.pem");
+		tls_context.use_private_key_file("D:/My Projects/Github/Sticky-notes.com/server_src/certificates/privkey.pem",
+			                             restinio::asio_ns::ssl::context::pem);
 
 		restinio::run(
 			restinio::on_this_thread<traits_t>()
@@ -34,7 +46,8 @@ int main()
 			.request_handler(
 				notesEndpoint.createNoteEndpointRequestHandler(),
 				mainEndpoint.createMainEndpointRequestHandler(),
-				userEndPoint.createUserEndpointRequestHandler()));
+				userEndPoint.createUserEndpointRequestHandler())
+			.tls_context(std::move(tls_context)));
 	}
 	catch (const std::exception& ex)
 	{
